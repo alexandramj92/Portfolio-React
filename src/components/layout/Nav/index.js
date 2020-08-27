40080841c93b1523bde7c3cb7c87e6734c1d9e8c
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-08-26 17:08:54 -0700

    converted to scss and fixed some issues

--- src/components/layout/Nav/index.js
+++ src/components/layout/Nav/index.js
@@ -2,44 +2,37 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import logo from '../../../assets/img/logo.png';
 import { navigate } from '@reach/router';
-
-
-require ('./style.css');
+import styles from './Nav.module.scss';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
+    '& header.MuiPaper-root.MuiAppBar-root.MuiAppBar-positionFixed.MuiAppBar-colorPrimary.mui-fixed.MuiPaper-elevation4': {
+        backgroundColor: 'white',
+        color: 'black',
+      },
+      '& div.MuiToolbar-root.MuiToolbar-regular.MuiToolbar-gutters':{
+        marginLeft: 'auto'
+      }
   },
 }));
 
 export default function ButtonAppBar() {
   const classes = useStyles();
-  
 
   const handleHome = event => {
     event.preventDefault();
     navigate('/');
   }
 
-
   const handlePort = event => {
     event.preventDefault();
     navigate('/portfolio');
   }
 
-  
   const handleAbout = event => {
     event.preventDefault();
     navigate('/about');
@@ -51,14 +44,14 @@ export default function ButtonAppBar() {
   }
 
   return (
-    <div id="nav" className={classes.root}>
+    <div id={styles.nav} className={classes.root}>
       <AppBar>
         <Toolbar>
-        <img id="nav-img" src={logo} />
-         <Button color="inherit" onClick={handleHome}>Home</Button>
-         <Button color="inherit" onClick={handlePort}>Projects</Button>
-         <Button color="inherit" onClick={handleAbout}>About</Button>
-         <Button color="inherit" onClick={handleContact}>Contact</Button>
+          <img alt="logo" id={styles.navImg} src={logo}/>
+          <Button color="inherit" onClick={handleHome}>Home</Button>
+          <Button color="inherit" onClick={handlePort}>Projects</Button>
+          <Button color="inherit" onClick={handleAbout}>About</Button>
+          <Button color="inherit" onClick={handleContact}>Contact</Button>
         </Toolbar>
       </AppBar>
     </div>
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -5,77 +5,50 @@ import Grid from '@material-ui/core/Grid';
 import aboutImg from '../../assets/img/about-img.jpg';
 import Palm from '../../assets/img/palm-tree-2.png';
 import SkillCard from '../../components/SkillCard/index';
-
-
-require ('./style.css');
-
-
+import styles from './About.module.scss';
 
 const About = () => {
 
-
     return (
-        <Container id="about-cont" maxWidth="xl">
+        <Container id={styles.aboutCont} maxWidth="xl">
+            <Paper elevation={3} id={styles.aboutPaper}>
+                <img alt="palm leaf" id={styles.palmOne} className={styles.palmImg} src={Palm} />
+                <img alt="palm leaf" id={styles.palmTwo} className={styles.palmImg} src={Palm} />
+                <img alt="palm leaf" id={styles.palmThree} className={styles.palmImg} src={Palm} />
+                <img alt="palm leaf" id={styles.palmFour} className={styles.palmImg} src={Palm} />
 
-        <Paper elevation={3} id="about-paper">
-        <img id="palm-one" className="palm-img" src={Palm} />
-        <img id="palm-two" className="palm-img" src={Palm} />
-        <img id="palm-three" className="palm-img" src={Palm} />
-        <img id="palm-four" className="palm-img" src={Palm} />
-
-        <Grid 
-    container 
-    spacing={2}
-    alignItems='center'
-    >
-        <Grid item xs={12}>
-        <h1>About Me</h1>
-        </Grid>
-
-        <Grid id="about-grid" item sm={12} md={6}>
-
-        <p>I was born in Paris, France and grew up in Malibu, California. I have always enjoyed gaining new skills, tackling complex challenges, and experiencing new things.
-        This has pushed me to do things like learn how to paint, ride horses, travel to over 20 countries, teach English in Spain, get my masters in France, and lastly learn how to code!
-        I am Full stack developer with a passion for solving problems. I love coding because it is the perfect balance between using my creativity and exercising my technical skills.
-        Previously, I worked for a nonprofit and successfully implemented tools that addressed major issues related to our financial and administrative processes. 
-        This spurred my desire to tackle more complex challenges leading to my enrollment in the 
-        Full Stack Web Development Coding Bootcamp at UCLA. I have developed various full-stack applications using MERN stack and integrating CRUD functionality. 
-        I am excited to continue on this journey and continue to develop my programming skills while creating web and mobile applications that solve real problems in an 
-        efficient way in order to positively influence our society and our planet.</p>
-         </Grid>
-
-
-       <Grid id="about-img-grid" item sm={12} md={6}>
-       <img id="about-img" src={aboutImg} />
-       </Grid>
-
-
-      <Grid item container xs={12} spacing={2}>
-      <Grid item xs={12}>
-          <h1>Skills</h1>
-      </Grid>
-      <SkillCard title='Frontend' skillType='frontend'/>
-      <SkillCard title='Backend' skillType='backend'/>
-      </Grid>
-
-
-
-
-
-  
-   
-      </Grid>
-
-        </Paper>
-
-        
+                <Grid 
+                    container 
+                    spacing={2}
+                    alignItems='center'
+                    >
+                    <Grid item xs={12}>
+                        <h1>About Me</h1>
+                    </Grid>
+                    <Grid id={styles.aboutGrid} item sm={12} md={6}>
+                        <p>I was born in Paris, France and grew up in Malibu, California. I have always enjoyed gaining new skills, tackling complex challenges, and experiencing new things.
+                        This has pushed me to do things like learn how to paint, ride horses, travel to over 20 countries, teach English in Spain, get my masters in France, and lastly learn how to code!
+                        I am Full stack developer with a passion for solving problems. I love coding because it is the perfect balance between using my creativity and exercising my technical skills.
+                        Previously, I worked for a nonprofit and successfully implemented tools that addressed major issues related to our financial and administrative processes. 
+                        This spurred my desire to tackle more complex challenges leading to my enrollment in the 
+                        Full Stack Web Development Coding Bootcamp at UCLA. I have developed various full-stack applications using MERN stack and integrating CRUD functionality. 
+                        I am excited to continue on this journey and continue to develop my programming skills while creating web and mobile applications that solve real problems in an 
+                        efficient way in order to positively influence our society and our planet.</p>
+                    </Grid>
+                    <Grid id={styles.aboutImgGrid} item sm={12} md={6}>
+                        <img alt="portrait of Alexandra Jackson" id={styles.aboutImg} src={aboutImg}/>
+                    </Grid>
+                    <Grid item container xs={12} spacing={2}>
+                        <Grid item xs={12}>
+                            <h1>Skills</h1>
+                        </Grid>
+                        <SkillCard title='Frontend' skillType='frontend'/>
+                        <SkillCard title='Backend' skillType='backend'/>
+                    </Grid>
+                </Grid>
+            </Paper>
         </Container>
-
-
     );
-
-
-
 }
 
 export default About;
