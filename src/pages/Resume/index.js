0735a9b5c1b390ba3fdcc1ebe4e1fc86d8b67f13
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-08-27 15:53:34 -0700

    added projects and footer comp

---
+++ src/pages/Resume/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Resume.module.scss';
+
+
+const Resume = () => {
+
+    return (
+            <iframe title="resume" src="https://resume.creddle.io/resume/hsdmjzmu6vl"
+            width="100%" height="1400px" seamless></iframe>
+    );
+}
+
+export default Resume;
